7ad596a8562a0ab0821403c6bd6671c5389776f2
K-ZX <163035740+K-ZX@users.noreply.github.com>
2024-12-09 17:56:13 -0500

    Update ConcreteSyntax.java

--- src/main/java/com/scanner/project/ConcreteSyntax.java
+++ src/main/java/com/scanner/project/ConcreteSyntax.java
@@ -1,315 +1,333 @@
-package com.scanner.project;
-// Implementation of the Scanner for KAY
+// ConcreteSyntax.java
 
+// Implementation of the Recursive Descent Parser algorithm
 
-// This code DOES NOT implement a scanner for JAY yet. You have to complete
-// the code and also make sure it implements a scanner for JAY - not something
+//  Each method corresponds to a concrete syntax grammar rule, 
+// which appears as a comment at the beginning of the method.
+
+// This code DOES NOT implement a parser for KAY. You have to complete
+// the code and also make sure it implements a parser for KAY - not something
 // else.
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+public class ConcreteSyntax {
 
-public class TokenStream {
-    
-    // READ THE COMPLETE FILE FIRST
-	// You will need to adapt it to KAY, NOT JAY
-    
-    // Instance variables 
-    private boolean isEof = false; // is end of file
-    private char nextChar = ' '; // next character in input stream
-    private BufferedReader input;
+	// READ THE COMPLETE FILE FIRST
+	
+	// Instance variables
+	public Token token; // current token that is considered from the input stream
+	public TokenStream input; // stream of tokens generated in by the lexical analysis
 
-    // This function was added to make the demo file work
-    public boolean isEoFile() {
-        return isEof;
-    }
-
-    // Constructor
-	// Pass a filename for the program text as a source for the TokenStream.
-    public TokenStream(String fileName) {
-        try {
-            input = new BufferedReader(new FileReader(fileName));
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found: " + fileName);
-            // System.exit(1); // Removed to allow ScannerDemo to continue
-			// running after the input file is not found.
-            isEof = true;
-        }
-    }
-
-    public Token nextToken() { // Main function of the scanner
-                                // Return next token type and value.
-        Token t = new Token();
-        t.setType("Other"); // For now it is Other
-        t.setValue("");
-        
-        // First check for whitespaces and bypass them
-        skipWhiteSpace();
-        if (isEof) return null;
-        
-        // Then check for a comment, and bypass it
-		// but remember that / may also be a division operator.
-        while (nextChar == '/') {
-            // Changed if to while to avoid the 2nd line being printed when
-			// there are two comment lines in a row.
-            nextChar = readChar();
-            if (nextChar == '/') { // If / is followed by another /
-                // skip rest of line - it's a comment.
-				// TODO TO BE COMPLETED W completed 
-                while((int)nextChar!=10&&(int)nextChar!=12&&(int)nextChar!=13) {
-                    nextChar = readChar();
-                }
-                // look for <cr>, <lf>, <ff>
-                skipWhiteSpace();
-                if(isEof) return null;
-            } 
-            else {
-                // A slash followed by anything else must be an operator.
-                t.setValue("/");
-                t.setType("Operator");
-                return t;
-            }
-        }
-        
-        // Then check for an operator; this part of the code should recover 2-character
-		// operators as well as 1-character ones.
-        if (isOperator(nextChar)) {
-            t.setType("Operator");
-            t.setValue(t.getValue() + nextChar);
-            
-            switch (nextChar) {
-            // TODO TO BE COMPLETED WHERE NEEDED
-                case ':':
-                    nextChar = readChar();
-                    
-                    if (nextChar == '=') {
-                        t.setValue(t.getValue() + nextChar);
-                        nextChar = readChar();
-                        return t;
-                    } else {
-                        t.setType("Other");
-                        nextChar=readChar();
-                    }
-                    return t;  
-                case '<':
-                    // <=
-                    nextChar = readChar();
-                    
-                    if (nextChar == '=') {
-                        t.setValue(t.getValue() + nextChar);
-                        nextChar = readChar();
-                        return t;
-                    } else {
-                        t.setValue("<");
-                    }
-                    return t;  
-                case '>':
-                    // >=
-                    nextChar = readChar();
-                    
-                    if (nextChar == '=') {
-                        t.setValue(t.getValue() + nextChar);
-                        nextChar = readChar();
-                        return t;
-                    } else {
-                        t.setValue(">");
-                    }
-                    return t;
-                case '=':
-                    // ==
-                    nextChar = readChar();
-                    
-                    if (nextChar == '=') {
-                        t.setValue(t.getValue() + nextChar);
-                        nextChar = readChar();
-                        return t;
-                    } else {
-                        t.setType("Other");
-                    }
-                    return t;
-                case '!':
-                    nextChar = readChar();
-                    
-                    if (nextChar == '=') {
-                        
-                        t.setValue(t.getValue() + nextChar);
-                        nextChar = readChar();
-                        return t;
-                    
-                    } else {
-                        t.setValue("!");
-                    }
-                    return t;
-                case '|':
-                    nextChar = readChar();
-                    
-                    if (nextChar == '|') {
-                        t.setValue(t.getValue() + nextChar);
-                        nextChar = readChar();
-                        return t;
-                    
-                    } else {
-                        t.setType("Other");
-                        nextChar=readChar();
-                    }
-                    return t;
-    
-                case '&':
-                    // Look or &&
-                    nextChar = readChar();
-                    
-                    if (nextChar == '&') {
-                        t.setValue(t.getValue() + nextChar);
-                        nextChar = readChar();
-                        return t;
-                    
-                    } else {
-                        t.setType("Other");
-                        nextChar=readChar();
-                    }
-                    return t;
-    
-                default:
-                    nextChar = readChar();
-                    return t;
-            }
-        }
-    
-        if (isSeparator(nextChar)) {
-            t.setType("Separator");
-            // TODO TO BE COMPLETED
-            t.setValue(t.getValue() + nextChar);
-            nextChar = readChar();
-            return t;
-        }
-    
-        if (isLetter(nextChar)) {
-            
-            t.setType("Identifier");
-            while (isLetter(nextChar) || isDigit(nextChar)) {
-                t.setValue(t.getValue() + nextChar);
-                nextChar = readChar();
-            }
-            
-            if (isKeyword(t.getValue())) {
-                t.setType("Keyword");
-            } else if (t.getValue().equals("True") || t.getValue().equals("False")) {
-                t.setType("Literal");
-            }
-            if (isEndOfToken(nextChar)){
-                return t;
-            }
-            
-        }
-    
-        if (isDigit(nextChar)) {
-            t.setType("Literal");
-            while (isDigit(nextChar)) {
-                t.setValue(t.getValue() + nextChar);
-                nextChar = readChar();
-            }
-           if (isEndOfToken(nextChar)) {
-            return t;
-        }
-    }
-        
-        t.setType("Other");
-        
-        if (isEof) {
-            return t;
-        }
-        
-        while (!isEndOfToken(nextChar)) {
-            t.setValue(t.getValue() + nextChar);
-            nextChar = readChar();
-        }
-
-        skipWhiteSpace();
-
-        return t;
-    }
-        
-
-    private char readChar() {
-        int i = 0;
-        if (isEof) 
-            return (char) 0;
-        System.out.flush();
-        try {
-            i = input.read();
-        } catch (IOException e) {
-            System.exit(-1);
-        }
-        if (i == -1) {
-            isEof = true;
-            return (char) 0;
-        }
-        return (char) i;
-    }
-
-    private boolean isKeyword(String s) {
-        if(s.equals("bool")||s.equals("else")||s.equals("if")||s.equals("integer")||s.equals("main")||s.equals("while"))
-		{
-			return true;
-		}
-		return false;
+	// Constructor
+	public ConcreteSyntax(TokenStream ts) { // Open the source program
+		input = ts; // as a TokenStream, and
+		token = input.nextToken(); // retrieve its first Token
 	}
 
-    private boolean isWhiteSpace(char c) {
-        return c == ' ' || c == '\t' || c == '\r' || c == '\n' || c == '\f';
-    }
-
-    private boolean isEndOfLine(char c) {
-        return c == '\r' || c == '\n' || c == '\f';
-    }
-
-    private boolean isEndOfToken(char c) {
-        return isWhiteSpace(nextChar) || isOperator(nextChar) || isSeparator(nextChar) || isEof;
-    }
-
-    private void skipWhiteSpace() {
-        // check for whitespaces, and bypass them
-        while (!isEof && isWhiteSpace(nextChar)) {
-            nextChar = readChar();
-        }
-    }
-
-    private boolean isSeparator(char c) {
-        // TODO TO BE COMPLETED
-        if (c=='('||c==')'||c=='{'||c=='}'||c==';'||c==',')
-		{
-			return true;
-		}
-		return false;
+	// Method that prints a syntax error message
+	private String SyntaxError(String tok) {
+		String s = "Syntax error - Expecting: " + tok + " But saw: "
+				+ token.getType() + " = " + token.getValue();
+		System.out.println(s);
+		return s;
+		// System.exit(0);
 	}
 
-    private boolean isOperator(char c) {
-        // Checks for characters that start operators
-		// TODO TO BE COMPLETED w completed 
-        if (c=='*'||c=='-'||c=='+'||c=='<'||c=='>'||c=='|'||c=='!'||c=='&'||c=='='||c=='/'||c==':')
-		{
-			return true;
-		}
-		return false;
+	// Match a string with the value of a token. If no problem, go to the next
+	// token otherwise generate an error message
+	private void match(String s) {
+		if (token.getValue().equals(s))
+			token = input.nextToken();
+		else
+			throw new RuntimeException(SyntaxError(s));
 	}
 
-    private boolean isLetter(char c) {
-        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
-    }
+	// Implementation of the Recursive Descent Parser
 
-    private boolean isDigit(char c) {
-        // TODO TO BE COMPLETED
-        if (c >= '0' && c <= '9')
-		{
-			return true;
+	public Program program() {
+		// TODO TO BE COMPLETED 
+		// Program --> main '{' Declarations Statements '}'
+		String[] header = {"main", "{" };
+		Program p = new Program();
+		for (int i = 0; i < header.length; i++){
+			// bypass " main { "
+			match(header[i]);
 		}
-		return false;
+		p.decpart = declarations();
+		p.body = statements();
+		match("}");
+		return p;
+	}
+		
+	
+
+	private Declarations declarations() {
+		// TODO TO BE COMPLETED 
+		// Declarations --> { Declaration }*
+		Declarations ds = new Declarations();
+		while (token.getValue().equals("integer")
+				|| token.getValue().equals("bool")) {
+			declaration(ds);
+		}
+		return ds;
 	}
 
-	public boolean isEndofFile() {
-		return isEof;
+	private void declaration(Declarations ds) {
+		// Declaration --> Type Identifiers ;
+		Type t = type();
+		identifiers(ds, t);
+		match(";");
+	}
+
+	private Type type() {
+		// TODO TO BE COMPLETED
+		// Type --> integer | bool
+		Type t = null;
+		if (token.getValue().equals("integer"))
+			t = new Type(token.getValue());
+		else if (token.getValue().equals("bool"))
+			t = new Type(token.getValue());
+		else
+			throw new RuntimeException(SyntaxError("integer | boolean"));
+		token = input.nextToken(); // pass over the type
+		return t;
+	}
+
+	private void identifiers(Declarations ds, Type t) {
+		// Identifiers --> Identifier { , Identifier }*
+		Declaration d = new Declaration(); // first declaration
+		d.t = t; // its type
+		if (token.getType().equals("Identifier")) {
+			d.v = new Variable();
+			d.v.id = token.getValue(); // its value
+			ds.addElement(d);
+			token = input.nextToken();
+			while (token.getValue().equals(",")) {
+				d = new Declaration(); // next declaration
+				d.t = t; // its type
+				token = input.nextToken();
+				if (token.getType().equals("Identifier")) {
+					d.v = new Variable(); // its value
+					d.v.id = token.getValue();
+					ds.addElement(d);
+					token = input.nextToken(); // get "," or ";"
+				} else
+					throw new RuntimeException(SyntaxError("Identifier"));
+			}
+		} else
+			throw new RuntimeException(SyntaxError("Identifier"));
+	}
+
+	private Statement statement() {
+		// Statement --> ; | Block | Assignment | IfStatement | WhileStatement
+		Statement s = new Skip();
+		if (token.getValue().equals(";")) { // Skip
+			token = input.nextToken();
+			return s;
+		} else if (token.getValue().equals("{")) { // Block
+			token = input.nextToken();
+			s = statements();
+			match("}");
+		} else if (token.getValue().equals("if")) // IfStatement
+			s = ifStatement();
+		else if (token.getValue().equals("while")) {
+			// WhileStatement
+			// TODO TO BE COMPLETED
+			s = whileStatement();
+		} else if (token.getType().equals("Identifier")) { // Assignment
+			// TODO TO BE COMPLETED
+			s=assignment();
+		} else
+			throw new RuntimeException(SyntaxError("Statement"));
+		return s;
+	}
+
+	private Block statements() {
+		// Block --> '{' Statements '}'
+		Block b = new Block();
+		while (!token.getValue().equals("}")) {
+			b.blockmembers.addElement(statement());
+		}
+		return b;
+	}
+
+	private Assignment assignment() {
+		// Assignment --> Identifier := Expression ;
+		Assignment a = new Assignment();
+		if (token.getType().equals("Identifier")) {
+			// TODO TO BE COMPLETED
+			a.target = new Variable();
+			a.target.id = token.getValue();
+			token = input.nextToken();
+			match(":=");
+			a.source = expression();
+		} else
+			throw new RuntimeException(SyntaxError("Identifier"));
+		return a;
+	}
+
+	private Expression expression() {
+		// Expression --> Conjunction { || Conjunction }*
+		Binary b;
+		Expression e;
+		e = conjunction();
+		while (token.getValue().equals("||")) {
+			b = new Binary();
+			b.term1 = e;
+			b.op = new Operator(token.getValue());
+			token = input.nextToken();
+			b.term2 = conjunction();
+			e = b;
+		}
+		return e;
+	}
+
+	private Expression conjunction() {
+		// Conjunction --> Relation { && Relation }*
+		Binary b;
+		Expression e;
+		e = relation();
+		while (token.getValue().equals("&&")) {
+			b = new Binary();
+			// TODO TO BE COMPLETED
+			b.term1 = e;
+			b.op = new Operator(token.getValue());
+			token = input.nextToken();
+			b.term2 = relation();
+			e = b;
+		}
+		return e;
+	}
+
+	private Expression relation() {
+		// Relation --> Addition [ < | <= | > | >= | == | <> ] Addition }*
+		Binary b;
+		Expression e;
+		e = addition();
+		// TODO TO BE COMPLETED
+		while (token.getValue().equals("<") || token.getValue().equals("<=")
+			|| token.getValue().equals(">") || token.getValue().equals(">=")
+				|| token.getValue().equals("==") 
+				|| token.getValue().equals("<>")) {
+			b = new Binary();
+			// TODO TO BE COMPLETED
+			b.term1 = e;
+			b.op = new Operator(token.getValue());
+			token = input.nextToken();
+			b.term2 = addition();
+			e = b;
+		}
+		return e;
+	}
+
+	private Expression addition() {
+		// Addition --> Term { [ + | - ] Term }*
+		Binary b;
+		Expression e;
+		e = term();
+		while (token.getValue().equals("+") || token.getValue().equals("-")) {
+			// TODO TO BE COMPLETED
+			b = new Binary();
+			b.term1 = e;
+			b.op = new Operator(token.getValue());
+			token = input.nextToken();
+			b.term2 = term();
+			e = b;
+		}
+		return e;
+	}
+
+	private Expression term() {
+		// Term --> Negation { [ '*' | / ] Negation }*
+		Binary b;
+		Expression e;
+		e = negation();
+		while (token.getValue().equals("*") || token.getValue().equals("/")) {
+			b = new Binary();
+			// TODO TO BE COMPLETED
+			b.term1=e;
+			b.op = new Operator(token.getValue());
+			token = input.nextToken();
+			b.term2=negation();
+			e = b;
+		}
+		return e;
+	}
+
+	private Expression negation() {
+		// Negation --> { ! }opt Factor
+		Unary u;
+		if (token.getValue().equals("!")) {
+			u = new Unary();
+			u.op = new Operator(token.getValue());
+			token = input.nextToken();
+			u.term = factor();
+			return u;
+		} else
+			return factor();
+	}
+
+	private Expression factor() {
+		// Factor --> Identifier | Literal | ( Expression )
+		Expression e = null;
+		if (token.getType().equals("Identifier")) {
+			Variable v = new Variable();
+			v.id = token.getValue();
+			e = v;
+			token = input.nextToken();
+		} else if (token.getType().equals("Literal")) {
+			Value v = null;
+			if (isInteger(token.getValue()))
+				v = new Value(Integer.parseInt(token.getValue()));
+			else if (token.getValue().equals("True"))
+				v = new Value(true);
+			else if (token.getValue().equals("False"))
+				v = new Value(false);
+			else
+				throw new RuntimeException(SyntaxError("Literal"));
+			e = v;
+			token = input.nextToken();
+		} else if (token.getValue().equals("(")) {
+			token = input.nextToken();
+			e = expression();
+			match(")");
+		} else
+			throw new RuntimeException(SyntaxError("Identifier | Literal | ("));
+		return e;
+	}
+
+	private Conditional ifStatement() {
+		// IfStatement --> if ( Expression ) Statement { else Statement }opt
+		Conditional c = new Conditional();
+		// TODO TO BE COMPLETED
+		match("if");
+		match("(");
+		c.test = expression();
+		match(")");
+		c.thenbranch = statement();
+		if(token.getValue().equals("else")) {
+			token = input.nextToken();
+			c.elsebranch = statement();
+		}
+		return c;
+	}
+
+	private Loop whileStatement() {
+		// WhileStatement --> while ( Expression ) Statement
+		Loop l = new Loop();
+		// TODO TO BE COMPLETED
+		match("while");
+		match("(");
+		l.test = expression();
+		match(")");
+		l.body = statement();
+		return l;
+	}
+
+	private boolean isInteger(String s) {
+		boolean result = true;
+		for (int i = 0; i < s.length(); i++)
+			if ('0' > s.charAt(i) || '9' < s.charAt(i))
+				result = false;
+		return result;
 	}
 }
-
